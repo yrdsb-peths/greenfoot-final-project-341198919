245d1b1a95655c51c708a84a2158be5e4c6a57f6
341198919 <341198919@gapps.yrdsb.ca>
2022-06-01 09:25:12 -0400

    StickFightGame v0.1
    Added sticks

--- MyWorld.java
+++ MyWorld.java
@@ -16,6 +16,31 @@ public class MyWorld extends World
     public MyWorld()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        super(600, 400, 1); 
+        super(900, 900, 1); 
+        Stick joe = new Stick();
+        addObject(joe,450,450);
+    }
+    
+    public void Enemy()
+    {
+        Enemy bob = new Enemy();
+        int sA = Greenfoot.getRandomNumber(4);
+        if(sA == 1)
+        {
+            addObject(bob,450,900);
+        }
+        if(sA == 2)
+        {
+            addObject(bob,900,450);
+        }
+        if(sA == 3)
+        {
+            addObject(bob,450,0);
+        }
+        if(sA == 4)
+        {
+            addObject(bob,0,450);
+        }
+        
     }
 }
---
+++ Stick.java
@@ -0,0 +1,31 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Stick here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Stick extends Actor
+{
+    /**
+     * Act - do whatever the Stick wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+
+    public void act()
+    {
+        if(Greenfoot.isKeyDown("w")){
+
+        }
+        if(Greenfoot.isKeyDown("a")){
+
+        }
+        if(Greenfoot.isKeyDown("s")){
+
+        }
+        if(Greenfoot.isKeyDown("d")){
+
+        }
+    }
+}
